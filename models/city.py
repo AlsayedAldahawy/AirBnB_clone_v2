c402cee3491018b0ae7ebf53c6a6504062e7b61a
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-29 23:42:55 +0300

    AirBnB_clone_v2 update

--- models/city.py
+++ models/city.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-
+from models.base_model import BaseModel, Base
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
