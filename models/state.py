76b11b558a7ed3502b468b8b7044007457f5db6d
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-07-06 20:12:36 +0300

    AirBnB_clone_v2 update

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -9,3 +10,23 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     id = Column()
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            '''
+
+            '''
+            from models import storage, city
+
+            City = city.City
+
+            city_list = []
+
+            for i in storage.all(City).values():
+                if i.state_id == self.id:
+                    city_list.append(i)
+            return city_list
+
